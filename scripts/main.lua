c82fc90f15bc26724c6533ebe24c3009fc40abb0
hotege <myownroc@163.com>
2019-10-08 18:13:51 +0800

    simplify scene initialize function

--- scripts/main.lua
+++ scripts/main.lua
@@ -2,8 +2,7 @@ function main()
     -- create window
     icon = loadImage(data.icon, data.encrypted)
     createWindow(icon)
-    scene_title.initialize()
-    scene.object = scene_title
+    scene.object = scene_title.initialize()
     -- main loop
     while (not windowShouldClose()) do
         if (scene.verify()) then
