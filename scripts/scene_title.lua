fa8a037f2072c421d962501c39360c76f98f90e2
hotege <myownroc@163.com>
2019-10-08 18:14:09 +0800

    load background in title

--- scripts/scene_title.lua
+++ scripts/scene_title.lua
@@ -1,9 +1,10 @@
 scene_title = {}
 
 function scene_title.initialize()
-    
+    background = loadImage(data.title.image, data.encrypted)
+    return scene_title
 end
 
 function scene_title.render()
-    
+
 end
