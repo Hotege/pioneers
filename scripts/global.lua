2dfe9c12df55343d7e8478d48c6de3362d38a129
hotege <myownroc@163.com>
2019-10-11 18:29:35 +0800

    use ":" to call functions

--- scripts/global.lua
+++ scripts/global.lua
@@ -8,10 +8,10 @@ This is a real-time tactics game engine, created by DL.
 You can contact me via myownroc@163.com.]=]
 global.version = "0.0.0"
 
-function global.getInfo()
+function global:getInfo()
     return global.name .. "\n" .. global.description .. "\n" .. "Current version: " .. global.version
 end
 
-function global.showInfo()
+function global:showInfo()
     message(global.getInfo())
 end
