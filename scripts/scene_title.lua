95bd1b557fa15711490a36c800238b5b57fc8a8c
hotege <myownroc@163.com>
2019-10-12 09:29:18 +0800

    declare functions in constructor

--- scripts/scene_title.lua
+++ scripts/scene_title.lua
@@ -2,19 +2,15 @@ scene_title = {}
 
 function scene_title:new(nilObject)
     self = {}
-    self.render = scene_title.render
-    self.release = scene_title.release
+    self.render = function()
+        clearScene(0.0, 0.5, 0.5, 1.0)
+        self.texture.background:draw(-1.0, -1.0, 1.0, 1.0)
+    end
+    self.release = function()
+        print("scene title release")
+    end
     self.background = loadImage(data.title.image, data.encrypted)
     self.texture = {}
     self.texture.background = self.background:generateTexture()
     return self
-end
-
-function scene_title:render()
-    clearScene(0.0, 0.5, 0.5, 1.0)
-    self.texture.background:draw(-1.0, -1.0, 1.0, 1.0)
-end
-
-function scene_title:release()
-    print("scene title release")
 end
