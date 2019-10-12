e32a0f9b27a3b890ab9ebafb7008e5a7936ebfc7
hotege <myownroc@163.com>
2019-10-12 13:49:09 +0800

    update structure initialize & use button

--- scripts/scene_title.lua
+++ scripts/scene_title.lua
@@ -1,16 +1,27 @@
 scene_title = {}
 
 function scene_title:new(nilObject)
-    self = {}
-    self.render = function()
-        clearScene(0.0, 0.5, 0.5, 1.0)
-        self.texture.background:draw(-1.0, -1.0, 1.0, 1.0)
-    end
-    self.release = function()
-        print("scene title release")
-    end
-    self.background = loadImage(data.title.image, data.encrypted)
-    self.texture = {}
-    self.texture.background = self.background:generateTexture()
+    self = {
+        texture = {
+            background = loadImage(data.title.image, data.encrypted):generateTexture(),
+        },
+        button = {
+            start = button:new(data.button.start.name),
+            continue = button:new(data.button.continue.name),
+            option = button:new(data.button.option.name),
+            exit = button:new(data.button.exit.name),
+        },
+        render = function()
+            clearScene(0.0, 0.5, 0.5, 1.0)
+            self.texture.background:display(-1.0, -1.0, 1.0, 1.0)
+            self.button.start:display()
+            self.button.continue:display()
+            self.button.option:display()
+            self.button.exit:display()
+        end,
+        release = function()
+            print("scene title release")
+        end,
+    }
     return self
 end
